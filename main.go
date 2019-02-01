803d148a55d7ad049f9e31c74f6531a9ad4d0984
Michele Pittoni <pittoni@spaziodati.eu>
2019-02-01 10:29:46 +0100

    Fix local imports

--- main.go
+++ main.go
@@ -11,9 +11,8 @@ import (
 
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
-
-	"./collectors"
-	"./parser"
+	"github.com/vad/elasticsearch_exporter/collectors"
+	"github.com/vad/elasticsearch_exporter/parser"
 )
 
 var (
